c1d315327fdfe6833e554b9a8606e9c119e0ab0f
Alessio <alessio.caciagli@okra.ai>
2021-04-06 09:09:48 +0100

    better commenting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bulk_renamer
- Bulk renamer with mapping dictionary
+ Bulk renamer with mapping dictionary.
+ Creates a copy of target directory and rename all files and subdirectories according to the mapping dictionary.
+
+ Use as following:
+ ```
+ python main.py folder_to_bulk_rename
+ ```
+
+ Note: the `main.py` and `mapping_dict.json` files MUST be in the same root directory as `folder_bulk_rename`
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import os
 import shutil
 import sys
 
+# Rename an item (file or dir) using a mapping dictionary
 def rename_item(item, mapping_dict):
     new_item = item
+    # Iterate over dict and map keys to values in new_item string
     for key, value in mapping_dict.items():
         new_item = new_item.replace(key, value)
 
+    # If file is being renamed
     if item != new_item:
         print('Renaming: ' + item + ' ------> ' + new_item)
         try:
@@ -17,12 +20,14 @@ def rename_item(item, mapping_dict):
             print(exc)
     pass
 
+# Process a directory
 def process_directory(directory, mapping_dict):
     for subdir,dirs,files in os.walk(directory):
+        # Process files
         for f in files:
             full_path = os.path.join(subdir, f)
             rename_item(full_path, mapping_dict=mapping_dict)
-
+        # Process subdirectories
         for d in dirs:
             full_path = os.path.join(subdir, d)
             rename_item(full_path, mapping_dict=mapping_dict)
@@ -31,24 +36,28 @@ def process_directory(directory, mapping_dict):
 
 
 def main():
+    # Load mapping dictionary
     with open("mapping_dict.json") as f:
         mapping_dict = json.load(f)
 
+    # Define target directory from command line
     if len(sys.argv) > 1 &  os.path.isdir(sys.argv[1]):
         print('Target: ' + sys.argv[1])
         print('')
     else:
         raise Exception('Directory not specified!')
     
+    # Define destination directory
     work_dir = sys.argv[1]
     dest_dir = work_dir + "_copy"
 
     if os.path.exists(dest_dir):
         raise Exception('Directory ' + dest_dir + ' exists already.')
 
-    # Create full copy of original directory
+    # Create full copy of original directory into destination directory
     shutil.copytree(work_dir, dest_dir)
     
+    # Process directory
     process_directory(dest_dir, mapping_dict=mapping_dict)
 
     
